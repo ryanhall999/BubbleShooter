43d6bb13db5a9ff688e3a7ef44578ea461c83211
Ryan Hall <ryanhall999@gmail.com>
2020-06-23 15:14:09 -0400

    bubble fires

---
+++ _js/bubble.js
@@ -0,0 +1,18 @@
+var BubbleShoot = window.BubbleShoot || {};
+
+BubbleShoot.Bubble = (function ($) {
+	var Bubble = function (sprite) {
+		var that = this;
+		this.getSprite = function () {
+			return sprite;
+		};
+	};
+	Bubble.create = function () {
+		var sprite = $(document.createElement("div"));
+		sprite.addClass("bubble");
+		sprite.addClass("bubble_0");
+		var bubble = new Bubble(sprite);
+		return bubble;
+	};
+	return Bubble;
+})(jQuery);
